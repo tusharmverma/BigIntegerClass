d87b404cb44e945917284791038d052a639a3dec
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 08:32:43 -0500

    New classes created in main file

--- BigInteger Class in C++/main.cpp
+++ BigInteger Class in C++/main.cpp
@@ -1,1 +1,29 @@
+#include "BigInt.hpp"
+#include <iostream>
 
+BigInt fibonacci(int n)
+{
+    BigInt a = 1;
+    BigInt b = 1;
+    BigInt placeholder;
+    for (int index = 2; index < n; index++)
+    {
+        placeholder = a;
+        a += b;
+        b = placeholder;
+    }
+    return a;
+}
+
+int main()
+{
+    BigInt a = "123456789";
+    BigInt b = "-123456789";
+    BigInt c = a + b;
+    a *= b;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    b = fibonacci(500);
+    std::cout << b;
+}
